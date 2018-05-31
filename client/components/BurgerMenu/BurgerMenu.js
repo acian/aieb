b097c64a7c755064715283a2fda8b601e1979385
rbriann <rbriann@gmail.com>
2018-05-31 12:46:48 -0300

    some changes

--- client/components/BurgerMenu/BurgerMenu.js
+++ client/components/BurgerMenu/BurgerMenu.js
@@ -2,11 +2,15 @@ import React, { Component, PropTypes } from 'react';
 import { slide as Menu } from 'react-burger-menu';
 
 //require('./BurgerMenu.css');
-import stylesBM from './BurgerMenu.css';
+//import stylesBM from './BurgerMenu.css';
 
 export class BurgerMenu extends Component {
+	showSettings (event) {
+	    event.preventDefault();
+  }
+
 	render () {
-		var stylesJs = {
+		var stylesBM = {
 		  bmBurgerButton: {
 		    position: 'fixed',
 		    width: '36px',
@@ -38,11 +42,26 @@ export class BurgerMenu extends Component {
 		  },
 		  bmOverlay: {
 		    background: 'rgba(0, 0, 0, 0.3)'
-		  }
+		  },
+		  option: {
+    		display: 'inline-block',
+    		margin: '0.75em',
+		    padding: '1.35em 1.1em',
+		    width: '15em',
+		    background: '#fffce1',
+		    textTransform: 'uppercase',
+		    letterSpacing: '1px',
+		    fontWeight: '800',
+		    borderTopLeftRadius: '20px 50px',
+		    borderTopRightRadius: '20px 50px',
+		    borderBottomRightRadius: '20px 50px',
+		    borderBottomLeftRadius: '20px 50px',
+		    cursor: 'pointer',
+		  },
 		}
 
 	    return (
-	      <Menu styles={stylesJs}>
+	      <Menu styles={stylesBM}>
 	        <a id="Students" className="menu-item" href="/">Home</a>
 	        <a id="about" className="menu-item" href="/">About</a>
 	        <a id="contact" className="menu-item" href="/">Contact</a>
