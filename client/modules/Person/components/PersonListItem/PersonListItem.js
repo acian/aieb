60e7dc487418ec96e19d1e412d3a38e64e9537a7
nelsonschonfeld <nelson_schonfeld@hotmail.com>
2018-06-13 18:22:15 -0300

    view list person paper

--- client/modules/Person/components/PersonListItem/PersonListItem.js
+++ client/modules/Person/components/PersonListItem/PersonListItem.js
@@ -2,12 +2,16 @@ import React, { PropTypes } from 'react';
 import { Link } from 'react-router';
 import { FormattedMessage } from 'react-intl';
 import Chip from '@material-ui/core/Chip';
-import DoneIcon from '@material-ui/icons/Done';
 import DeleteIcon from '@material-ui/icons/Delete';
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
 import EditIcon from '@material-ui/icons/ModeEdit';
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import Divider from '@material-ui/core/Divider';
+import ExpansionPanelActions from '@material-ui/core/ExpansionPanelActions';
+
 
 // Import Style
 import styles from './PersonListItem.css';
@@ -19,30 +23,39 @@ function handleClick() {
 function PersonListItem(props) {
   let descriptionPerson = '';
   descriptionPerson += props.person.email ? `${props.person.email}  /  ` : '';
-  descriptionPerson += props.person.cellphone ? `${props.person.cellphone}  /  ` : '';
-  descriptionPerson += props.person.address ? `${props.person.address} ` : '';
-
+  descriptionPerson += props.person.cellphone ? `${props.person.cellphone}  ` : '';
 
   return (
     <div>
-      <Paper elevation={9} className={styles['paper-description']} >
-        <div className={styles['actions-right']}>
+      <ExpansionPanel className={styles['paper-description']}>
+        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+          <div className={styles['primiry-heading']}>
+            <strong>{props.person.surname} , {props.person.name}</strong>
+          </div>
+          <div className={styles['secondary-heading']}>
+            <p >{descriptionPerson}</p>
+          </div>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          {Object.keys(props.person).map(function (key) {
+            if ((['birthDate', 'profession', 'professionPlace', 'address'].indexOf(key) >= 0) && (props.person[key].length > 0)) {
+              if (key === 'birthDate') {
+                return <Chip label={props.person[key].substr(0, 10)} />;
+              }
+              return <Chip label={props.person[key]} />;
+            }
+          })}
+        </ExpansionPanelDetails>
+        <Divider />
+        <ExpansionPanelActions>
           <Button onClick={handleClick} mini variant="fab" color="primary" aria-label="Editar">
             <EditIcon />
           </Button>
           <Button onClick={handleClick} mini variant="fab" color="secondary" aria-label="delete">
             <DeleteIcon />
           </Button>
-        </div>
-        <Typography variant="headline" component="h6">
-          <Link to={`/people/${props.person.dni}`} >
-            {props.person.surname}, {props.person.name}
-          </Link>
-        </Typography>
-        <Typography component="p">
-          {descriptionPerson}
-        </Typography>
-      </Paper>
+        </ExpansionPanelActions>
+      </ExpansionPanel>
     </div>
   );
 }
