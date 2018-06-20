dfe7e94e6cc72036b40739bef15612f3da38e8b6
rbriann <rbriann@gmail.com>
2018-06-20 10:36:33 -0300

    icon to menu

--- client/modules/Person/components/PersonCreateWidget/PersonCreateWidget.js
+++ client/modules/Person/components/PersonCreateWidget/PersonCreateWidget.js
@@ -4,6 +4,7 @@ import TextField from '@material-ui/core/TextField';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 
+
 // Import Style
 import styles from './PersonCreateWidget.css';
 
